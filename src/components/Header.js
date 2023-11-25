8df5fea589f2e2a1fddb84fe651f863e58d86118
Kou <ykou@itc-imac-25.ur.rochester.edu>
2023-11-25 16:02:56 -0500

    add rough job alert

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,10 +3,12 @@ import { NavLink, useNavigate } from 'react-router-dom';
 import { useAuth } from '../auth/AuthProvider';
 import logo from '../assets/logo.png';
 import '../styles/header.css';
+import JobAlert from "./JobAlert";
 
 const Header = () => {
     const navigate = useNavigate();
     const [ showDropdown, setShowDropdown ] = useState(false);
+    const [isJobAlertOpen, setIsJobAlertOpen] = useState(false);
     const { isLoggedIn, user, logout } = useAuth();
 
     const handleLogoutClick = () => {
@@ -16,49 +18,56 @@ const Header = () => {
         return isLoggedIn && user?.roles?.includes('ADMIN');
     };
 
+    const openJobAlert = () => setIsJobAlertOpen(true);
+    const closeJobAlert = () => setIsJobAlertOpen(false);
+
 
     return (
-        <header className="header">
-            <div className="container">
-                <div className="logoAndTitle">
-                    <img src={logo} alt="AMES Logo" className="logo" />
-                    <NavLink to="/" className="appTitle ml-2">JobPortal</NavLink>
+        <>
+            <header className="header">
+                <div className="container">
+                    <div className="logoAndTitle">
+                        <img src={logo} alt="AMES Logo" className="logo" />
+                        <NavLink to="/" className="appTitle ml-2">JobPortal</NavLink>
+                    </div>
+                    <nav className="navLinks">
+                        <NavLink to="/software-newgrad" className="navLink"><span role="img" aria-label="Software NewGrad">👩🏻‍💻</span> Software NewGrad</NavLink>
+                        <NavLink to="/software-intern" className="navLink"><span role="img" aria-label="Software NewGrad">👩‍💻</span> Software Intern</NavLink>
+                        <NavLink to="/data-fulltime" className="navLink"><span role="img" aria-label="Software NewGrad">🙋‍♂️</span> Data Full-Time</NavLink>
+                        <NavLink to="/data-intern" className="navLink"><span role="img" aria-label="Software NewGrad">🙋</span> Data Intern</NavLink>
+                    </nav>
+                    <div>
+                        {isLoggedIn ? (
+                            <div
+                                className="user-menu"
+                                onMouseOver={() => setShowDropdown(true)}
+                                onMouseLeave={() => setShowDropdown(false)}
+                            >
+                                <span className="username">
+                                    Welcome! {user?.username}
+                                </span>
+                                {showDropdown && (
+                                    <div className="dropdown-menu">
+                                        <div className="dropdown-item" onClick={() => navigate('/dashboard')}>Dashboard</div>
+                                        {isAdmin() && ( // Check if the user is an admin before displaying this option
+                                            <div className="dropdown-item" onClick={() => navigate('/add-job-post')}>Add job post</div>
+                                        )}
+                                        <div className="dropdown-item" onClick={openJobAlert}>Job Alert</div>
+                                        <div className="dropdown-item" onClick={handleLogoutClick}>Log Out</div>
+                                    </div>
+                                )}
+                            </div>
+                        ) : (
+                            <>
+                                <button onClick={() => navigate('/login')} className="loginBtn">Log In</button>
+                                <button onClick={() => navigate('/register')} className="loginBtn">Register</button>
+                            </>
+                        )}
+                    </div>
                 </div>
-                <nav className="navLinks">
-                    <NavLink to="/software-newgrad" className="navLink"><span role="img" aria-label="Software NewGrad">👩🏻‍💻</span> Software NewGrad</NavLink>
-                    <NavLink to="/software-intern" className="navLink"><span role="img" aria-label="Software NewGrad">👩‍💻</span> Software Intern</NavLink>
-                    <NavLink to="/data-fulltime" className="navLink"><span role="img" aria-label="Software NewGrad">🙋‍♂️</span> Data Full-Time</NavLink>
-                    <NavLink to="/data-intern" className="navLink"><span role="img" aria-label="Software NewGrad">🙋</span> Data Intern</NavLink>
-                </nav>
-                <div>
-                    {isLoggedIn ? (
-                        <div
-                            className="user-menu"
-                            onMouseOver={() => setShowDropdown(true)}
-                            onMouseLeave={() => setShowDropdown(false)}
-                        >
-                            <span className="username">
-                                Welcome! {user?.username}
-                            </span>
-                            {showDropdown && (
-                                <div className="dropdown-menu">
-                                    <div className="dropdown-item" onClick={() => navigate('/dashboard')}>Dashboard</div>
-                                    {isAdmin() && ( // Check if the user is an admin before displaying this option
-                                        <div className="dropdown-item" onClick={() => navigate('/add-job-post')}>Add job post</div>
-                                    )}
-                                    <div className="dropdown-item" onClick={handleLogoutClick}>Log Out</div>
-                                </div>
-                            )}
-                        </div>
-                    ) : (
-                        <>
-                            <button onClick={() => navigate('/login')} className="loginBtn">Log In</button>
-                            <button onClick={() => navigate('/register')} className="loginBtn">Register</button>
-                        </>
-                    )}
-                </div>
-            </div>
-        </header>
+            </header>
+        <JobAlert isOpen={isJobAlertOpen} onClose={closeJobAlert} />
+    </>
     );
 };
 
